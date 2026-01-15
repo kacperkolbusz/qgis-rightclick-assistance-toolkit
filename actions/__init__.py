f024775aa2e5f1225d054ae58e173100235365ff
Kacper Kolbusz <kacperkolbusz@pm.me>
2026-01-15 12:29:53 +0100

    init

--- actions/__init__.py
+++ actions/__init__.py
@@ -1,11 +1,1 @@
-"""
-Actions package for Right-click Utilities and Shortcuts Hub
-
-This package contains individual action modules for the right-click context menu.
-Each action is implemented as a separate Python file for better organization
-and easier maintenance.
-"""
-
 from .base_action import BaseAction
-
-__all__ = ['BaseAction']
